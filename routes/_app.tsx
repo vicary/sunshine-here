794ec72619f55dcdcd5a6a658f6fff1b3efc6d90
Vicary A. <vicary.archangel@member.mensa.org>
2025-12-19 05:16:58 +0800

    feat: Add site manifest and favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -24,6 +24,26 @@ export default define.page(function App({ Component, state }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{state.title ?? "Sunshine Here"}</title>
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#d97706" />
         <script
           src="https://kit.fontawesome.com/55ef370e6a.js"
           crossorigin="anonymous"
